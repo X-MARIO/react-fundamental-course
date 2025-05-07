fdb4683246595862b33c013f6bb0465d51db817e
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-05-07 21:50:09 +0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+function App() {
+    const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState('');
+
+    const increment = () => {
+        setLikes(likes + 1);
+    };
+
+    const decrement = () => {
+        setLikes(likes - 1);
+    };
+
+    return (
+        <div>
+            <h1>{likes}</h1>
+            <h1>{value}</h1>
+            <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+
+export default App
